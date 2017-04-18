533bdbcff2972f76235fad0eb43d7f9b444b7cce
Martin Rehder <martin@Rehsco-MacPro.local>
2017-04-18 12:30:27 +0200

    macOS header added

---
+++ ImagePersistenceMac/ImagePersistenceMac.h
@@ -0,0 +1,19 @@
+//
+//  ImagePersistenceMac.h
+//  ImagePersistenceMac
+//
+//  Created by Martin Rehder on 18.04.2017.
+//  Copyright © 2017 Martin Jacob Rehder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImagePersistenceMac.
+FOUNDATION_EXPORT double ImagePersistenceMacVersionNumber;
+
+//! Project version string for ImagePersistenceMac.
+FOUNDATION_EXPORT const unsigned char ImagePersistenceMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImagePersistenceMac/PublicHeader.h>
+
+
